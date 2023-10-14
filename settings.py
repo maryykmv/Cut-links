c7a4235e85673a522e633643e8d6434d16af54ac
Mary K <wildcat.3333@yandex.ru>
2023-10-14 19:00:51 +0700

    chages dir for test

--- settings.py
+++ settings.py
@@ -1,11 +1,7 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__)) + '/yacut_app'
-
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = (os.getenv('TYPE_DB') +
-                               os.path.join(basedir,
-                                            os.getenv('NAME_DB')))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
