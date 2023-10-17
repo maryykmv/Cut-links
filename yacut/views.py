a7f1177d7dc6045d660c6d7e3e0190fda0f0e6b9
Mary K <wildcat.3333@yandex.ru>
2023-10-18 01:34:58 +0700

    review3 try except

--- yacut/views.py
+++ yacut/views.py
@@ -3,7 +3,7 @@ from flask import flash, redirect, render_template, url_for
 from . import app
 
 from .constants import (INDEX_TEMPLATE, INDEX_TEMPLATE,
-                        MESSAGE_EXISTS_SHORT, REDIRECT_VIEW)
+                        REDIRECT_VIEW)
 from .forms import URLMapForm
 from .models import URLMap
 
@@ -14,15 +14,18 @@ MESSAGE_CREATE_URL = 'Ваша новая ссылка готова:'
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLMapForm()
-    if form.validate_on_submit():
-        if form.custom_id.data is None or form.custom_id.data == '':
-            url_map = URLMap().data(short=None, url=form.original_link.data)
-        else:
-            url_map = URLMap().data(form.custom_id.data, url=form.original_link.data)
-        form.custom_id.data = url_map['short_link']
-        flash(MESSAGE_CREATE_URL)
-        short_url = url_for(
-            REDIRECT_VIEW, short=form.custom_id.data, _external=True)
+    try:
+        if form.validate_on_submit():
+            if form.custom_id.data is None or form.custom_id.data == '':
+                url_map = URLMap().data(short=None, url=form.original_link.data)
+            else:
+                url_map = URLMap().data(short=form.custom_id.data, url=form.original_link.data)
+            flash(MESSAGE_CREATE_URL)
+            form.custom_id.data = url_map['short_link']
+            short_url = url_for(
+                REDIRECT_VIEW, short=form.custom_id.data, _external=True)
+    except Exception:
+        pass
     return render_template(INDEX_TEMPLATE, **locals())
 
 
